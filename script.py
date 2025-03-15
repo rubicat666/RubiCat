ec7432ba4b670f51865209d02d1f39c47fd0b7af
rubicat666 <cintiahrubi2008@gmail.com>
2025-03-15 15:19:44 -0600

    Primer actualizacion

---
+++ script.py
@@ -0,0 +1,18 @@
+print('hola desde un script de python')
+
+nombre = 'cinthya'
+edad = 16
+pi = 3.1416
+es_mayor = True
+print(nombre , edad ,pi, es_mayor,edad+5)
+
+# operaciones matematicas
+a = 10
+b = 3
+print(a + b) # suma
+print(a - b) # resta
+print(a * b) # multiplicacin
+print(a / b) # divicion
+print(a // b) # divicion entera
+print(a % b) #modulo
+print(a ** b) # potencia
